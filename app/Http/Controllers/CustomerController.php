e7e5822f2fe893ecb8e8e708a6b7ca2d16fb8ebe
xiz860817 <s0984008299@gmail.com>
2020-01-22 13:33:33 +0800

    CustomerController.php

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Route;
+use View;
 class CustomerController extends Controller
 {
-    //
+  public function index(){
+          return View::make('board');
+  }
 }
