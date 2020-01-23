ec1e46f3e1949956d6e16fac6fd285e820b8b87d
xiz860817 <s0984008299@gmail.com>
2020-01-23 08:58:35 +0800

    TEST

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,7 +5,7 @@
     </a>
     <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a href="{{ action('CustomerController@index') }}" class="nav-link">
+        <a href="{{ action('CarsController@index') }}" class="nav-link">
           客戶列表
         </a>
       </li>
