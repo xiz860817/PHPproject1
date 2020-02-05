3e50ca59862228436ffb99c227fc597d0212fe0e
xiz860817 <s0984008299@gmail.com>
2020-02-05 12:30:00 +0800

    migration

---
+++ database/migrations/2020_02_05_042639_create_customers_table.php
@@ -0,0 +1,26 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class CreateCustomersTable extends Migration
+{
+  public function up()
+  {
+    Schema::create('customers', function (Blueprint $table)
+    {
+      //$table->bigIncrements('id'); 這欄位會形成 Primary Key
+      $table->bigIncrements('id');
+      $table->char('Cusid',100)->unique()->index();
+      $table->char('Name',50)->index();
+      $table->char('Address',255);
+      $table->char('Phone',10)->index();
+      //協助建立追踪的時間
+      $table->timestamps();
+    });
+  }
+  
+  public function down()
+  {
+    Schema::dropIfExists('customer');
+  }
+}
