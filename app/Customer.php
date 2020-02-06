987d58080995fff390bfd09fdfcb2ed2d0d570ee
xiz860817 <s0984008299@gmail.com>
2020-02-06 21:57:37 +0800

    新增資料

--- app/Customer.php
+++ app/Customer.php
@@ -10,4 +10,12 @@ class Customer extends Model
     protected $table = 'customers';
     protected $primarykey = 'id';
     public $timestamps = true;
+    // 從 View 送過來的　$request 內容會是陣列，看起來如下：
+    $post = App\Customer::create([
+        'Name' => 'Merry',
+        'Address' => 'Test Load No.12',
+        'Phone' => '0912345678'
+    ]);
+    // 可用下列方法，節省輸入的時間
+    $post = App\Customer::create($request->all());
 }
